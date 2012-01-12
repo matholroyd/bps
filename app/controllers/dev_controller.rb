dbc028d19e53928dca577675ae2c47625f85a001
Mat Holroyd <matholroyd@gmail.com>
2012-01-12 22:43:08 +1100

    Added some more dev-actions only in development mode

--- app/controllers/dev_controller.rb
+++ app/controllers/dev_controller.rb
@@ -1,6 +1,10 @@
 class DevController < ApplicationController
-  def blank_slate
-    User.destroy_all
-    redirect_to root_path
+  if Rails.env.development?
+    
+    def blank_slate
+      User.destroy_all
+      redirect_to root_path
+    end
+    
   end
 end
