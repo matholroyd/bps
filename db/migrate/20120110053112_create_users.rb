f6afd383d4363c4403c6014957a80c5b3ed8201e
Mat Holroyd <matholroyd@gmail.com>
2012-01-12 21:16:22 +1100

    Added simple user model

---
+++ db/migrate/20120110053112_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email,            length: 150,  null: false
+      t.string :password_hash,    length: 60,   null: false
+      t.string :password_salt,    length: 29,   null: false
+
+      t.timestamps
+    end
+  end
+end
