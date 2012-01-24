d1c603aaca84c686640db00da5db56dce303be55
Mat Holroyd <matholroyd@gmail.com>
2012-01-24 17:19:47 +1100

    Failing spec for user + site

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,6 @@
 class HomeController < ApplicationController
   def index
-    unless User.exists?
-      redirect_to new_user_path
-    end
+    redirect_to new_user_path
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe User do
+  describe 'validations' do
+    it 'blueprint should be valid' do
+      User.make
+    end
+
+    %w{email password}.each do |field|
+      it "requires #{field}" do
+        User.make_unsaved(field => nil).should_not be_valid
+      end
+    end
+  end
+  
+  describe 'setup?' do
+    it do
+      User.should_not be_setup
+    end
+    
+    it do 
+      User.make
+      User.should be_setup
+    end
+  end
   
 end
