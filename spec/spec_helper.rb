dbaee27d01e1a04df070f65198a5767689389590
Mat Holroyd <matholroyd@gmail.com>
2012-01-27 11:25:35 +1100

    Made specs reset properly, a js tests connect properly to DB

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,4 +16,26 @@ RSpec.configure do |config|
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
 
+  config.before(:all)    { Sham.reset(:before_all)  }
+  config.before(:each)   { 
+    Sham.reset(:before_each) 
+    ActionMailer::Base.deliveries.clear
+  }
 end
+
+
+# Forces all threads to share the same connection. This works on
+# Capybara because it starts the web server in a thread.
+#
+# See https://groups.google.com/forum/#!msg/ruby-capybara/JI6JrirL9gM/R6YiXj4gi_UJ
+# and http://pastie.org/1745020
+class ActiveRecord::Base
+  mattr_accessor :shared_connection
+  @@shared_connection = nil
+
+  def self.connection
+    @@shared_connection || retrieve_connection
+  end
+end
+
+ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
