6c81d9d14e15a8b454bc46f7cc06756ddc4d0875
Mat Holroyd <matholroyd@gmail.com>
2012-02-08 17:47:20 +1100

    Tweaking styling of gravatar image

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -3,7 +3,7 @@ require 'digest/md5'
 module UserHelper
   def gravatar_url(user)
     hash = Digest::MD5.hexdigest(user.email.strip.downcase)
-    "http://www.gravatar.com/avatar/#{hash}"
+    "http://www.gravatar.com/avatar/#{hash}.jpg?s=50&d=mm"
   end
   
   
--- spec/helpers/user_helper_spec.rb
+++ spec/helpers/user_helper_spec.rb
@@ -4,6 +4,6 @@ describe UserHelper do
   it "should return the gravatar url" do
     # Example from Gravatar
     user = User.make(email: "MyEmailAddress@example.com")
-    helper.gravatar_url(user).should == "http://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346"
+    helper.gravatar_url(user).should == "http://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346.jpg?s=50&d=mm"
   end
 end
