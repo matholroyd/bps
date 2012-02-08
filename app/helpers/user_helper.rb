b5af1182e0d4cefc96b388a7cd9282e4a7195c24
Mat Holroyd <matholroyd@gmail.com>
2012-02-08 22:35:27 +1100

    Turn off Gravatar for acceptance specs

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,12 +1,16 @@
 require 'digest/md5'
 
 module UserHelper
-  def gravatar_url
-    if User.first
-      hash = Digest::MD5.hexdigest(User.first.email.strip.downcase)
-      "https://secure.gravatar.com/avatar/#{hash}.jpg?s=50&d=mm"
-    else
-      "https://secure.gravatar.com/avatar/xxx.jpg?s=50&d=mm&f=y"
+  def gravatar_url(options = {})
+    # Showing gravatar slows down acceptance specs, so turn off, but allow
+    # manually turning on for heplers specs
+    if !Rails.env.test? || options[:on] 
+      if User.first
+        hash = Digest::MD5.hexdigest(User.first.email.strip.downcase)
+        "https://secure.gravatar.com/avatar/#{hash}.jpg?s=50&d=mm"
+      else
+        "https://secure.gravatar.com/avatar/xxx.jpg?s=50&d=mm&f=y"
+      end
     end
   end
   
--- spec/helpers/user_helper_spec.rb
+++ spec/helpers/user_helper_spec.rb
@@ -4,12 +4,12 @@ describe UserHelper do
   it "should return the gravatar url" do
     # Example from Gravatar
     User.make(email: "MyEmailAddress@example.com")
-    helper.gravatar_url.should == "https://secure.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346.jpg?s=50&d=mm"
+    helper.gravatar_url(on: true).should == "https://secure.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346.jpg?s=50&d=mm"
   end
   
   it 'should return default image if no user' do
     # Example from Gravatar
-    helper.gravatar_url.should == "https://secure.gravatar.com/avatar/xxx.jpg?s=50&d=mm&f=y"
+    helper.gravatar_url(on: true).should == "https://secure.gravatar.com/avatar/xxx.jpg?s=50&d=mm&f=y"
     
   end
 end
