1a9fcf0f0fb54e40a4282051940dd8dca10c5556
Mat Holroyd <matholroyd@gmail.com>
2012-02-08 18:04:32 +1100

    Tweaked gravatar image usage to show default even if no user setup

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,9 +1,13 @@
 require 'digest/md5'
 
 module UserHelper
-  def gravatar_url(user)
-    hash = Digest::MD5.hexdigest(user.email.strip.downcase)
-    "http://www.gravatar.com/avatar/#{hash}.jpg?s=50&d=mm"
+  def gravatar_url
+    if User.first
+      hash = Digest::MD5.hexdigest(User.first.email.strip.downcase)
+      "https://secure.gravatar.com/avatar/#{hash}.jpg?s=50&d=mm"
+    else
+      "https://secure.gravatar.com/avatar/xxx.jpg?s=50&d=mm&f=y"
+    end
   end
   
   
--- spec/helpers/user_helper_spec.rb
+++ spec/helpers/user_helper_spec.rb
@@ -3,7 +3,13 @@ require 'spec_helper'
 describe UserHelper do
   it "should return the gravatar url" do
     # Example from Gravatar
-    user = User.make(email: "MyEmailAddress@example.com")
-    helper.gravatar_url(user).should == "http://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346.jpg?s=50&d=mm"
+    User.make(email: "MyEmailAddress@example.com")
+    helper.gravatar_url.should == "https://secure.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346.jpg?s=50&d=mm"
+  end
+  
+  it 'should return default image if no user' do
+    # Example from Gravatar
+    helper.gravatar_url.should == "https://secure.gravatar.com/avatar/xxx.jpg?s=50&d=mm&f=y"
+    
   end
 end
