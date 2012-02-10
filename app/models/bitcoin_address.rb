cfb10d7bca9d30a2fb3701fbefd1994c692fa013
Mat Holroyd <matholroyd@gmail.com>
2012-02-10 23:18:01 +1100

    Copied over good default sorting for bitcoin address

--- app/models/bitcoin_address.rb
+++ app/models/bitcoin_address.rb
@@ -3,5 +3,6 @@ class BitcoinAddress < ActiveRecord::Base
   validates :public_key,  presence: true
   validates :private_key, presence: true
   validates :description, presence: true
-  
+
+  default_scope order: 'updated_at DESC'
 end
