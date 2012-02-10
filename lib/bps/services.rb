4fbe568e60713791bce718e998f9aff871f77194
Mat Holroyd <matholroyd@gmail.com>
2012-02-11 00:57:54 +1100

    Added service to find random record

--- lib/bps/services.rb
+++ lib/bps/services.rb
@@ -5,6 +5,16 @@ module BPS
         chars = (('a'..'z').to_a + ('0'..'9').to_a) - %w(i o 0 1 l 0)
         (1..size).collect{|a| chars[rand(chars.size)] }.join
       end    
+      
+      def random(model)
+        choices = model.limit(5)
+        if choices.count > 0
+          choices[rand(choices.count)]
+        else
+          nil
+        end
+      end
+      
     end
   end
 end
