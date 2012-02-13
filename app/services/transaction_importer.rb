9e95adb3e1a56aba9389c064e6d2175341dc0e09
Mat Holroyd <matholroyd@gmail.com>
2012-02-13 14:19:38 +1100

    Refactored a little

--- app/services/transaction_importer.rb
+++ app/services/transaction_importer.rb
@@ -2,35 +2,41 @@ class TransactionImporter
   class << self
 
     def pull_transactions(bitcoin_addresses)
-      DBC.require(*bitcoin_addresses)
+      DBC.require(bitcoin_addresses)
       
-      json = Api::BlockExplorer.mytransactions bitcoin_addresses
+      extract_transactions Api::BlockExplorer.mytransactions(bitcoin_addresses),  bitcoin_addresses 
+    end
+    
+    def extract_transactions(json, bitcoin_addresses)
+      json.collect do |k, transaction_json|
+        extract_transaction(json, bitcoin_addresses, transaction_json)
+      end
+    end
+    
+    def extract_transaction(json, bitcoin_addresses, tx_hash)
+      transaction = Transaction.new
+      tx = Bitcoin::Protocol::Tx.from_hash(tx_hash)
 
-      json.collect do |k, hash_data|
-        transaction = Transaction.new
-        tx = Bitcoin::Protocol::Tx.from_hash(hash_data)
-
-        tx.out.each do |tx_out|
-          addr = Bitcoin::Script.new(tx_out.pk_script).get_hash160_address
-          if bitcoin_addresses.include? addr
-            ba = BitcoinAddress.new address: addr
-            amount = BigDecimal(tx_out.value) / (10**8)
-            transaction.payments.build amount: amount, bitcoin_address: ba
-          end
-        end
-
-        tx.in.select { |tx_in| json[tx_in.previous_output].present? }.each do |tx_in|
-          node = json[tx_in.previous_output]['out'][tx_in.prev_out_index]
-
-          addr = node['address'] || Bitcoin::Script.new(node['scriptPubKey']).get_address
+      tx.out.each do |tx_out|
+        addr = Bitcoin::Script.new(tx_out.pk_script).get_address
+        if bitcoin_addresses.include? addr
           ba = BitcoinAddress.new address: addr
-          amount = -BigDecimal(node['value'])
-          
+          amount = BigDecimal(tx_out.value) / (10**8)
           transaction.payments.build amount: amount, bitcoin_address: ba
         end
-        
-        transaction
       end
+
+      tx.in.select { |tx_in| json[tx_in.previous_output].present? }.each do |tx_in|
+        node = json[tx_in.previous_output]['out'][tx_in.prev_out_index]
+
+        addr = node['address'] || Bitcoin::Script.new(node['scriptPubKey']).get_address
+        ba = BitcoinAddress.new address: addr
+        amount = -BigDecimal(node['value'])
+        
+        transaction.payments.build amount: amount, bitcoin_address: ba
+      end
+      
+      transaction
     end
  
   end
