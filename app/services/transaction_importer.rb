39a4156ac725ac183c5288dc3bd94dd997668174
Mat Holroyd <matholroyd@gmail.com>
2012-02-13 15:32:48 +1100

    Refactoring specs + service to better handle import_for

--- app/services/transaction_importer.rb
+++ app/services/transaction_importer.rb
@@ -1,29 +1,31 @@
 class TransactionImporter
   class << self
+    def import_for(bitcoin_address)
+    end
 
-    def pull_transactions(bitcoin_addresses)
-      DBC.require(bitcoin_addresses)
+    def pull_transactions(addresses)
+      DBC.require(addresses)
       
-      extract_transactions Api::BlockExplorer.mytransactions(bitcoin_addresses),  bitcoin_addresses 
+      extract_transactions Api::BlockExplorer.mytransactions(addresses),  addresses 
     end
     
-    def pull_transaction(hash, bitcoin_addresses)
-      extract_transaction Api::BlockExplorer.rawtx(hash), bitcoin_addresses
+    def pull_transaction(hash, addresses)
+      extract_transaction Api::BlockExplorer.rawtx(hash), addresses
     end
     
-    def extract_transactions(json, bitcoin_addresses)
+    def extract_transactions(json, addresses)
       json.collect do |k, transaction_json|
-        extract_transaction transaction_json, bitcoin_addresses, json
+        extract_transaction transaction_json, addresses, json
       end
     end
     
-    def extract_transaction(tx_hash, bitcoin_addresses, json = {})
+    def extract_transaction(tx_hash, addresses, json = {})
       transaction = Transaction.new
       tx = Bitcoin::Protocol::Tx.from_hash(tx_hash)
 
       tx.out.each do |tx_out|
         addr = Bitcoin::Script.new(tx_out.pk_script).get_address
-        if bitcoin_addresses.include? addr
+        if addresses.include? addr
           ba = BitcoinAddress.new address: addr
           amount = BigDecimal(tx_out.value) / (10**8)
           transaction.payments.build amount: amount, bitcoin_address: ba
--- spec/services/transaction_importer_spec.rb
+++ spec/services/transaction_importer_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe TransactionImporter do
-  let(:address_nothing)     { "1BDnQ3UCwTTkL4jKLZabaiu9qd9566kJKf" }
-  let(:address_paid_then_spend)  { "1JDfUiJHZ6pDY6wWYTx86RYjDCW7QxCofs" }
-  let(:address_in_and_out)  { "1VayNert3x1KzbpzMGt2qdqrAThiRovi8" }
+  let(:internal_address)  { "1JDfUiJHZ6pDY6wWYTx86RYjDCW7QxCofs" }
+  let(:internal_private_key) { "1e2e0bc6893d42a462b0039b5c15c3da3378c8d0ec44556b9608efdb2b3caff1" }
   let(:address_several_transactions)  { "1G8A6rRugWuqGpXpRKBip1DpVVUV9KtALK" }
 
   describe "pull_transaction" do
+    
     # Something wrong with this transaction...even block explorer seems to have trouble
     # viewing it (viewing the address on block explorer causes 'Gateway Timeout')
     # it "should handle transaction same in and out address" do
@@ -29,12 +29,15 @@ describe TransactionImporter do
   end
   
   describe "pull_transactions" do
+    let(:address_nothing)     { "1BDnQ3UCwTTkL4jKLZabaiu9qd9566kJKf" }
+    let(:address_in_and_out)  { "1VayNert3x1KzbpzMGt2qdqrAThiRovi8" }
+    
     it "should return nil if no transactions" do
       TransactionImporter.pull_transactions(address_nothing).should == []
     end
 
     it "pulls raw data into transactions/payments/addresses" do
-      txs = TransactionImporter.pull_transactions address_paid_then_spend
+      txs = TransactionImporter.pull_transactions internal_address
       txs.count.should == 2
 
       txs[0].should be_new_record
@@ -44,14 +47,14 @@ describe TransactionImporter do
       txs[0].payments[0].amount.should == 0.1
       
       txs[0].payments[0].bitcoin_address.should be_new_record
-      txs[0].payments[0].bitcoin_address.address.should == address_paid_then_spend
+      txs[0].payments[0].bitcoin_address.address.should == internal_address
 
       txs[1].payments.length.should == 1
       txs[1].payments[0].should be_new_record
       txs[1].payments[0].amount.should == -0.1
 
       txs[1].payments[0].bitcoin_address.should be_new_record
-      txs[1].payments[0].bitcoin_address.address.should == address_paid_then_spend
+      txs[1].payments[0].bitcoin_address.address.should == internal_address
     end
     
     it "can handle bunches of transactions" do
@@ -61,6 +64,21 @@ describe TransactionImporter do
       txs.collect(&:payments).flatten.inject(BigDecimal('0')) do |sum, payment|
         sum + payment.amount
       end.should == BigDecimal("5.43045")
+    end
+  end
+  
+  describe "import" do
+    let(:bitcoin_address) { BitcoinAddress.make private_key: internal_private_key }
+    
+    before :each do
+      # @ba = 
+      # 
+      # Bitcoin::Key.new(private_key).addr.should == address_internal
+      
+    end
+    
+    it "should create the transactions and payments" do
+      TransactionImporter.import_for bitcoin_address
     end
   end
 end
