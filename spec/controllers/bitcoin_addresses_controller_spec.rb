73d59515b6f1ed3cd75a999cb64233df57996177
Mat Holroyd <matholroyd@gmail.com>
2012-02-14 22:09:43 +1100

    Refactored spec a bit

--- spec/controllers/bitcoin_addresses_controller_spec.rb
+++ spec/controllers/bitcoin_addresses_controller_spec.rb
@@ -1,21 +1,33 @@
 require 'spec_helper'
 
 describe BitcoinAddressesController do
-  let(:bitcoin_address) { BitcoinAddress.make }
+
+  describe '#create' do
+    it "should create a " do
+      
+    end
+  end
 
   describe '#show' do
-    it do
+    let(:bitcoin_address) { BitcoinAddress.make }
+
+    before :each do
       get :show, id: bitcoin_address.id_alias, format: :json
-      data = JSON.parse(response.body)
-      
-      %w{id_alias address public_key description}.each do |field|
-        data[field].to_s.should == bitcoin_address.send(field).to_s
-      end
-      
-      %w{id private_key}.each do |field|
-        data[field].should be_nil
+      @data = JSON.parse(response.body)
+    end
+    
+    %w{id_alias address public_key description}.each do |field|
+      it "should have #{field}" do
+        @data[field].to_s.should == bitcoin_address.send(field).to_s
       end
     end
+    
+    %w{id private_key}.each do |field|
+      it "should not have #{field}" do
+        @data[field].should be_nil
+      end
+    end
+    
   end
 
 end
