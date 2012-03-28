e068768520da72a01ad474d1627c14f7c556d7c8
Mat Holroyd <matholroyd@gmail.com>
2012-03-27 23:10:44 -0700

    TransactionTransmitter uses bitsend.rowit.co.uk

--- lib/api/transaction_transmitter.rb
+++ lib/api/transaction_transmitter.rb
@@ -1,8 +1,24 @@
+require 'open-uri'
+
 module Api
   class TransactionTransmitter
+    Domain = "http://bitsend.rowit.co.uk"
+    
     class << self
       def transmit(tx)
+        do_transmit tx.to_payload.unpack('H*').first
       end
+      
+      private
+      
+      def do_transmit(data_in_hex)
+        if Rails.env.test?
+          # do nothing
+        else
+          open("#{Domain}/?transaction=#{data_in_hex}")
+        end
+      end
+      
     end
   end
 end
